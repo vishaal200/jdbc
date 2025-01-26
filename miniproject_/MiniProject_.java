a78d39063bbde5c5015c2e7c292085de7324f8b7
vishaal200 <134786273+vishaal200@users.noreply.github.com>
2025-01-26 23:58:07 +0530

    Add files via upload

---
+++ miniproject_/MiniProject_.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miniproject_;
+
+/**
+ *
+ * @author USER
+ */
+public class MiniProject_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Pharmacysupply p=new Pharmacysupply();
+        p.setVisible(true);
+        
+    }
+    
+}
